ede81453e628c62dcaa4ca1ffa4afdf041523d77
Nikita Vorontsov <nikvoron3008@gmail.com>
2026-09-23 18:31:56 +0300

    feat: add PUT endpoint to products

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
 from app.models.products import Product as ProductModel
@@ -55,6 +55,7 @@ async def get_products_by_category(category_id: int, db: Session = Depends(get_d
     db_products = db.scalars(
         select(ProductModel).where(ProductModel.category_id == category_id, ProductModel.is_active == True)
     ).all()
+
     return db_products
 
 @router.get("/{product_id}", response_model=ProductSchema)
@@ -67,14 +68,35 @@ async def get_product(product_id: int, db: Session = Depends(get_db)):
     ).first()
     if db_product is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found or inactive")
+    
     return db_product
 
-@router.put("/{product_id}")
-async def update_product(product_id: int):
+@router.put("/{product_id}", response_model=ProductSchema)
+async def update_product(product_id: int, product: ProductCreate, db: Session = Depends(get_db)):
     """
     Обновляет товар по его ID.
     """
-    return {"message": f"Товар {product_id} обновлён (заглушка)"}
+    db_product = db.scalars(
+        select(ProductModel).where(ProductModel.id == product_id, ProductModel.is_active == True)
+    ).first()
+    if db_product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found or inactive")
+
+    db_category = db.scalars(
+        select(CategoryModel).where(CategoryModel.id == product.category_id, CategoryModel.is_active == True)
+    ).first()
+    if db_category is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category not found or inactive")
+
+    db.execute(
+        update(ProductModel)
+        .where(ProductModel.id == product_id)
+        .values(**product.model_dump())
+    )
+    db.commit()
+    db.refresh(db_product)
+        
+    return db_product
 
 @router.delete("/{product_id}")
 async def delete_product(product_id: int):
